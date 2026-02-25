efd7b22deb3ff8efed11868716a297166499e3d0
ssirec <125913769+ssirec@users.noreply.github.com>
2026-02-25 14:46:53 +0100

    Create gls.plugin.ts

---
+++ src/gls.plugin.ts
@@ -0,0 +1,26 @@
+// gls.plugin.ts
+
+import { PluginCommonModule, VendurePlugin } from '@vendure/core';
+import { GlsService } from './gls.service';
+
+export interface GlsPluginOptions {
+  username: string;
+  password: string; // plain text, SHA512 se naredi interno
+  clientNumber: number;
+  country: 'si' | 'hr' | 'hu' | 'cz' | 'sk' | 'ro' | 'rs';
+  printerType?: string;
+  webshopEngine?: string;
+}
+
+@VendurePlugin({
+  imports: [PluginCommonModule],
+  providers: [GlsService],
+})
+export class GlsPlugin {
+  static options: GlsPluginOptions;
+
+  static init(options: GlsPluginOptions) {
+    this.options = options;
+    return GlsPlugin;
+  }
+}
